7d2d1b1df44330141d1a6117413f546236cb7fad
Communitea <76811080+XYZ12715@users.noreply.github.com>
2022-05-26 18:10:15 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,16 @@
 local plr = game:GetService("Players").LocalPlayer
---SettingsStart
-local truth = false
---SettingsEnd
-
-rconsolename('Prison Life GUI - Synapse X Edition: ' ..plr.Name)
-rconsoleprint('@@GREEN@@')
-rconsoleprint('Autoupdater included!')
-rconsoleprint('@@BLUE@@')
-rconsoleprint('  Log: ')
+--Information
+local beingupdated = false
+--InformationEnd
+if(beingupdated == false) then
+    rconsolename('Prison Life GUI - Synapse X Edition: ' ..plr.Name)
+    rconsoleprint('@@CYAN@@')
+    rconsoleprint('Autoupdater included!')
+    rconsoleprint('@@BLUE@@')
+    rconsoleprint('  Log: ')
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/XYZ12715/prisonlife/main/plgui.lua'),true))()
-
-if(truth == true) then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/XYZ12715/prisonlife/main/truth.lua'),true))()
     else
-        
-end
+    rconsolename('Prison Life GUI - Synapse X Edition: ' .."BEING UPDATED")
+    rconsoleprint('@@RED@@')
+    rconsoleprint('THE GUI IS BEING UPDATED! CANNOT EXECUTE.')
+    end
