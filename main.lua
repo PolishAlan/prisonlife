3f160a23bb0e500f66feaa98b94a21743e4fe61c
Communitea <76811080+XYZ12715@users.noreply.github.com>
2022-05-27 20:31:33 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,22 @@
 local plr = game:GetService("Players").LocalPlayer
+local prisonlife = game.PlaceId
 --Information
 local beingupdated = false
 local discord = "None"
 --InformationEnd
-if(beingupdated == false) then
-    rconsolename('Prison Life GUI - Synapse X Edition: ' ..plr.Name)
-    rconsoleprint('@@CYAN@@')
-    rconsoleprint('Autoupdater included!  Discord: '..discord ..'  Roblox: no')
-    rconsoleprint('@@BLUE@@')
-    rconsoleprint('  Log: ')
-    game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Prison Life GUI",
-	Text = "Holy shit! You actually have Synapse X (or any other paid exploit)", -- Required
-	Icon = "rbxassetid://9731610495"
+	if(prisonlife == 155615604) then
+		if(beingupdated == false) then
+                rconsolename('Prison Life GUI - Synapse X Edition: ' ..plr.Name)
+                rconsoleprint('@@CYAN@@')
+                rconsoleprint('Autoupdater included!  Discord: '..discord ..'  Roblox: no')
+                rconsoleprint('@@BLUE@@')
+                rconsoleprint('  Log: ')
+                game:GetService("StarterGui"):SetCore("SendNotification",{
+	        Title = "Prison Life GUI",
+	        Text = "Holy shit! You actually have Synapse X (or any other paid exploit)", -- Required
+	        Icon = "rbxassetid://9731610495"
 })
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/XYZ12715/prisonlife/main/plgui.lua'),true))()
+          loadstring(game:HttpGet(('https://raw.githubusercontent.com/XYZ12715/prisonlife/main/plgui.lua'),true))()
     else
     rconsolename('Prison Life GUI - Synapse X Edition: ' .."BEING UPDATED")
     rconsoleprint('@@RED@@')
@@ -24,6 +26,10 @@ loadstring(game:HttpGet(('https://raw.githubusercontent.com/XYZ12715/prisonlife/
 	Text = "Oh no, the gui is being updated",
 	Icon = "rbxassetid://9731610495"
 })
+	else
+		rconsoleprint('@@RED@@')
+    rconsoleprint('YOURE NOT PLAYING PRISON LIFE! CANNOT EXECUTE.')
+		plr:Kick('YOURE NOT PLAYING PRISON LIFE! CANNOT EXECUTE.')
     end
 print 'Prison Life GUI - Synapse X Edition: Custom Launcher loaded'
 -- Prison Life GUI - Synapse X Edition: Custom Launcher
